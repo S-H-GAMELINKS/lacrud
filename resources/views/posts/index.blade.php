ae6128353a13810f8b5d7afed0fed25066fbf9f3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-22 13:47:01 +0900

    Delete Post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,12 @@
 @foreach($posts as $post)
-    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+    <p>
+        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a> : 
+        <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <button type="submit">Delete</button>
+        </form>
+    </p>
 @endforeach
 
 <a href="/posts/create">New Post</a>
